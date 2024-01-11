0dc9ec1e2fd418f13ab13e61bec7dbf51cd85817
Kostadin Urumov <koceurumov@gmail.com>
2024-01-11 22:18:12 +0000

    saved

---
+++ Airport_App/Areas/Manager/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airport_App.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Route("/Manager/[controller]/[Action]/{id?}")]
+    [Authorize(Policy = "AdminsOnly")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
--- Airport_App/Program.cs
+++ Airport_App/Program.cs
@@ -79,9 +79,20 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+
+    );
+});
+
+
 app.MapRazorPages();
 
 app.Run();
